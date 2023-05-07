c656f05128d6b6efb4b9992e2d4cb43d8e46c1d1
Konstantin Khaletskiy <41511242+konstkhal@users.noreply.github.com>
2023-05-07 12:16:05 +0300

    fix(withRoot): Fix tslint error

--- src/onepirate/modules/withRoot.tsx
+++ src/onepirate/modules/withRoot.tsx
@@ -3,16 +3,18 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "./theme";
 
-export default function withRoot<P>(Component: React.ComponentType<P>) {
-  function WithRoot(props: P) {
-    return (
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...props} />
-      </ThemeProvider>
-    );
-  }
+export default function withRoot<P extends JSX.IntrinsicAttributes>(
+    Component: React.ComponentType<P>,
+) {
+    function WithRoot(props: P) {
+        return (
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                <Component {...props} />
+            </ThemeProvider>
+        );
+    }
 
-  return WithRoot;
+    return WithRoot;
 }
